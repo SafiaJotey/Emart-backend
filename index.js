ee580404a083b07647a209a5073c828fdcd64702
Safia Jotey <safiaahmed1786@gmail.com>
2022-05-28 18:42:35 +0600

    data base connected

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xldcc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connecte successfully');
+  } finally {
+  }
+}
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+//});
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Addidas Running');
